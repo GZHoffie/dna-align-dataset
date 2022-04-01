9f16a26b7b80edae02f96d80e0668e71c917055d
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2022-04-01 10:49:13 +0800

    update script

--- get_CIGAR.sh
+++ get_CIGAR.sh
@@ -3,14 +3,14 @@
 while getopts ":h" flag
 do
     case "${flag}" in
-        h) echo "Usage: $0 <sam_file> <fasta_file>"
+        h) echo "Usage: $0 <sam_file>"
            exit;;
     esac
 done
 
 
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <sam_file> <fasta_file>"
+    echo "Usage: $0 <sam_file>"
     exit 9
 fi
 
@@ -23,8 +23,7 @@ tail -n +3 $sam_file | sed "s/\*/-/g" | while read line
 do
     line_split=($line)
     if [ "${line_split[5]}" != "-" ]; then
-        echo "<${line_split[9]}"
-        echo ">$(samtools faidx sequence.fasta ${line_split[2]}:${line_split[3]}-$((${line_split[3]} + ${#line_split[9]} - 1)) | tail -n +2 | tr -d \\n)"
+        echo "${line_split[5]}"
     fi
 done
 
